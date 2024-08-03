a50f99511c7b6d263da37207419109ffc3bc0665
harshit Mishra <miananya2@gmail.com>
2024-08-04 00:09:03 +0530

    ai-landing_hero

--- ai-landing/src/sections/Hero.tsx
+++ ai-landing/src/sections/Hero.tsx
@@ -3,16 +3,43 @@ import starsBg from "@/assets/stars.png";
 
 export const Hero = () => {
   return (
+    // whole Hero section style has star image getting repeated left and right
     <section
-      className="h-[492px] flex items-center"
+      className="h-[492px] md:h-[680px] flex items-center relative overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
       style={{ backgroundImage: `url(${starsBg.src})` }}
     >
-      <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
-      <div className="container relative">
-        <h1 className="text-8xl font-semibold tracking-tighter text-center bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text">
+      {/* purple background */}
+      <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
+      {/* planet with it's shadow */}
+      <div className="absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border border-white/10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+      {/* inner most ring */}
+      <div className="absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] border border-white rounded-full top-1/2 left-1/2 opacity-20 -translate-x-1/2 -translate-y-1/2">
+        {/* left dot */}
+        <div className="absolute h-2 w-2 top-1/2 -translate-y-1/2 -translate-x-1/2 left-0 rounded-full bg-white"></div>
+        {/* upper dot */}
+        <div className="absolute h-2 w-2 top-0 -translate-y-1/2 -translate-x-1/2 left-1/2 rounded-full bg-white"></div>
+        {/* right dot */}
+        <div className="absolute h-5 w-5 top-1/2 -translate-y-1/2 -translate-x-1/2 left-full rounded-full border border-white inline-flex items-center justify-center">
+          {/* inner dot */}
+          <div className="h-2 w-2 bg-white rounded-full"></div>
+        </div>
+      </div>
+      {/* second inner ring */}
+      <div className="absolute h-[444px] opacity-20 w-[444px] md:h-[780px] md:w-[780px] border border-white rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></div>
+      {/* 3rd inner ring */}
+      <div className="absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {/* left dot */}
+        <div className="absolute h-2 w-2 top-1/2 -translate-y-1/2 -translate-x-1/2 left-0 rounded-full bg-white"></div>
+        {/* right dot */}
+        <div className="absolute h-5 w-5 top-1/2 -translate-y-1/2 -translate-x-1/2 left-full rounded-full border bg-white"></div>
+      </div>
+      {/* text container */}
+      <div className="container relative mt-16">
+        {/* gradient heading text */}
+        <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter text-center bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text">
           AI SEO
         </h1>
-        <p className="text-lg text-white/70 mt-5 text-center">
+        <p className="text-lg md:text-xl max-w-xl mx-auto text-white/70 mt-5 text-center">
           Elevate your site&apos;s visibility effortlessly with AI, when smart
           technology meets user-friendly SEO tools.
         </p>
