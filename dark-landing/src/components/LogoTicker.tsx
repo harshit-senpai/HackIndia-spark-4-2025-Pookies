4d2e46d945cbbaea84a649064c127e0e8713cf88
harshit Mishra <miananya2@gmail.com>
2024-08-22 16:29:27 +0530

    dark-landing_features

--- dark-landing/src/components/LogoTicker.tsx
+++ dark-landing/src/components/LogoTicker.tsx
@@ -20,10 +20,10 @@ export const LogoTicker = () => {
   return (
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
-        <h2 className="text-xl text-center text-white70">
+        <h2 className="text-xl text-center text-white/70">
           Trusted by the world&apos;s most innovative teams
         </h2>
-        <div className="overflow-hidden mt-9 before:contrast-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+        <div className="flex items-center justify-center overflow-hidden mt-9 before:contrast-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
           <div className="flex gap-16">
             {images.map((image) => (
               <Image
