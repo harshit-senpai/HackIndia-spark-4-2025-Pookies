6d2de4b53715454d9fce79d7f53b6fd8b3a946d8
harshit Mishra <miananya2@gmail.com>
2024-09-01 04:58:30 +0530

    dark-landing_footer

--- dark-landing/src/components/Footer.tsx
+++ dark-landing/src/components/Footer.tsx
@@ -1,3 +1,28 @@
+import XSocial from "../assets/icons/x-social.svg";
+import TiktokIcon from "../assets/icons/tiktok.svg";
+import YoutubeIcon from "../assets/icons/youtube.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20 ">
+      <div className="container">
+        <div className="flex flex-col sm:flex-row sm:justify-between gap-4">
+          <div className="text-center">
+            &copy; 2024 Your Company Name, All rights reserved.
+          </div>
+          <ul className="flex justify-center gap-2.5">
+            <li>
+              <XSocial />
+            </li>
+            <li>
+              <TiktokIcon />
+            </li>
+            <li>
+              <YoutubeIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
